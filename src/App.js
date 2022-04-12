47cfe339509223b272cc2a45ea6f9e0173973fcf
riky moroni <riky.moroni5@gmail.com>
2022-04-12 15:04:30 +0100

    add basic app setup and homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <h1>Quizzical</h1>
+        <p>How knowledgeable are you? Answer to 5 questions and let's find out 😊</p>
+        <button>Start Quiz</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
