58b33a115030018beaeba0aea6c20f9ab6d7d3e1
riky moroni <riky.moroni5@gmail.com>
2022-04-12 15:54:47 +0100

    create homepage component and add 2 images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import Homepage from './components/Homepage'
+import questionMark from './images/question_mark.jpg'
 
 function App() {
   return (
     <div className="App">
-      <section>
-        <h1>Quizzical</h1>
-        <p>How knowledgeable are you? Answer to 5 questions and let's find out 😊</p>
-        <button>Start Quiz</button>
-      </section>
+      <img className='qm-left' src={questionMark} alt="question_mark" />
+      <Homepage />
+      <img className='qm-right' src={questionMark} alt="question_mark" />
     </div>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import './Homepage.css'
+
+export default function Homepage() {
+  
+  return (
+    <section>
+      <h1>Quizzical</h1>
+      <p>How knowledgeable are you? Answer to 5 questions and let's find out 😊</p>
+      <button>Start Quiz</button>
+    </section>
+  )
+}
