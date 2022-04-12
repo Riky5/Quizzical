43944c454e1954bc752a6adf9ec5534ef8a6a8a0
riky moroni <riky.moroni5@gmail.com>
2022-04-12 16:11:35 +0100

    add function setQuiz, when button is clicked the homepage disappear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { useState } from 'react'
 import Homepage from './components/Homepage'
 import questionMark from './images/question_mark.jpg'
 
 function App() {
+  const [quiz, setQuiz] = useState(false)
+
+  function startQuiz() {
+    setQuiz(true)
+  }
+
   return (
     <div className="App">
       <img className='qm-left' src={questionMark} alt="question_mark" />
-      <Homepage />
+      {!quiz && <Homepage startQuiz={startQuiz} />}
       <img className='qm-right' src={questionMark} alt="question_mark" />
     </div>
   );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,11 @@
 import './Homepage.css'
 
-export default function Homepage() {
-  
+export default function Homepage(props) {
   return (
     <section>
       <h1>Quizzical</h1>
       <p>How knowledgeable are you? Answer to 5 questions and let's find out 😊</p>
-      <button>Start Quiz</button>
+      <button onClick={props.startQuiz}>Start Quiz</button>
     </section>
   )
 }
