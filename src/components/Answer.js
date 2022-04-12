8097afad9b1614023181b52e43247e44d711d867
riky moroni <riky.moroni5@gmail.com>
2022-04-12 23:51:08 +0100

    randomize the order of the answers, add selectAnswer function, when clicking on asnwer it is selected and state changes, add conditional rendering

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,9 +1,13 @@
 import './Answer.css'
 
 export default function Answer(props) {
+  const styles = {
+    backgroundColor: props.isSelected ? "blue" : "white",
+    color: props.isSelected ? "white" : "black"
+  }
   return (
     <div>
-      <button className='answer-btn'>{props.a}</button>
+      <button onClick={props.selectAnswer} className='answer-btn' style={styles}>{props.a}</button>
     </div>
   )
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,24 +1,56 @@
 import './Question.css'
 import Answer from '../components/Answer'
 import { useEffect, useState } from 'react'
+import { nanoid } from 'nanoid'
 
 export default function Question(props) {
   const [answers, setAnswers] = useState([])
 
   useEffect(()=>{
     const answerArray = []
-    props.data.incorrect_answers.forEach(answer=>{answerArray.push(answer)})
-    answerArray.push(props.data.correct_answer)
-
-    setAnswers(answerArray)
+    props.data.incorrect_answers.forEach((answer)=>answerArray.push({
+      id: nanoid(),
+      answer: answer,
+      isSelected: false
+    }))
+    answerArray.push({
+      id: nanoid(),
+      answer: props.data.correct_answer,
+      isSelected: false
+    })
+    const randomized = randomizeAnswers(answerArray)
+    setAnswers(randomized)
   },[])
   
-  const answerData = answers.map((answer,i)=>{
+  function randomizeAnswers(arr) {
+    return [...arr].map( (_, i, arrCopy) => {
+      let rand = i + ( Math.floor( Math.random() * (arrCopy.length - i) ) );
+      [arrCopy[rand], arrCopy[i]] = [arrCopy[i], arrCopy[rand]]
+      return arrCopy[i]
+    })
+  }
+  
+  const answerData = answers.map((answer)=>{
     return (
-      <Answer key={i} a={answer} />
+      <Answer 
+      key={answer.id} 
+      a={answer.answer}
+      isSelected={answer.isSelected}
+      selectAnswer={()=>selectAnswer(answer.id)} 
+      />
     )
   })
 
+  function selectAnswer(id) {
+    const newAnswers = answers.map(answer=>{
+      if (answer.id === id){
+        answer.isSelected = !answer.isSelected
+      }
+      return answer
+    })
+    setAnswers(newAnswers)
+  }
+  
   return (
     <div className='question'>
       <h3>{props.data.question}</h3>
